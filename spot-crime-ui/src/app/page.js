581d2d151cb962fb5dad46383575fe68c2c88805
gadarajainik <gadarajainik1@gmail..com>
2024-10-13 12:46:17 -0500

    pipeline trigger

--- spot-crime-ui/src/app/page.js
+++ spot-crime-ui/src/app/page.js
@@ -25,7 +25,6 @@ const theme = createTheme({
 export default function Home() {
   const [openDialog, setOpenDialog] = useState(false);
 
-  const handleOpenDialog = () => setOpenDialog(true);
   const handleCloseDialog = () => setOpenDialog(false);
 
   return (
