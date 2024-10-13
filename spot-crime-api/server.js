ea0e99788ba5c887c34977db82ecc6a99bcdeb08
gadarajainik <gadarajainik1@gmail..com>
2024-10-12 20:58:53 -0500

    Google place API integration

--- spot-crime-api/server.js
+++ spot-crime-api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
+const axios = require('axios');
+
 
 // Initialize express app
 const app = express();
@@ -46,6 +48,38 @@ app.get('/api/crimecategory', async (req, res) => {
     res.status(500).json({ error: 'Database error'});
   }
 });
+
+app.get('/api/autocomplete', async (req, res) => {
+  const input = req.query.input;
+  const location = "34.05327442582769,-118.26310435834333"; // Los Angeles
+  const radius = 50000;
+
+  try {
+      const response = await axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURIComponent(input)}&location=${location}&radius=${radius}&strictbounds=true&key=AIzaSyCGCejPxj93O5lcGEezTVJ7QhO6YvC-oMw`);
+      response.data.predictions = response.data.predictions.map(prediction => {
+          return {
+              description: prediction.description,
+              placeId: prediction.place_id
+          }
+      });
+      res.json(response.data);  // Send the response back to the frontend
+  } catch (error) {
+    console.log(error)
+      res.status(500).send(JSON.stringify(error));
+  }
+});
+
+app.post('/api/crimes', async (req, res) => {
+  try {
+    const placeId = req.body.placeId;
+    const respone = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=AIzaSyCGCejPxj93O5lcGEezTVJ7QhO6YvC-oMw`)
+    const location = respone.data.result.geometry.location;
+    const result = await pool.query('SELECT DISTINCT crm_cd_desc FROM californiacrime'); // Replace 'your_table' with the table you're querying
+  } catch (error) {
+    res.status(500).send(JSON.stringify(error));
+  }
+});
+
 
 // Start the server
 app.listen(PORT, () => {
